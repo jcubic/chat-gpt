15e16eaf9624741c20acfe749252c8e91be7bcdb
Jakub Jankiewicz <jcubic@onet.pl>
2023-01-08 11:34:45 +0100

    add try..catch #7

--- bookmark.js
+++ bookmark.js
@@ -1,15 +1,16 @@
 javascript:(function() {
-  const a = document.createElement('a');
-  const dom = document.querySelector('main > .flex-1 > .h-full .flex:has(> .w-full)');
-  const template = document.createElement('template');
-  const title = document.title;
-  template.innerHTML = dom.innerHTML;
-  ['.items-end', 'img', 'svg', 'button', ':empty'].forEach(selector => {
-    template.content.querySelectorAll(selector).forEach(node => {
-      node.remove();
+  try {
+    const a = document.createElement('a');
+    const dom = document.querySelector('main > .flex-1 > .h-full .flex:has(> .w-full)');
+    const template = document.createElement('template');
+    const title = document.title;
+    template.innerHTML = dom.innerHTML;
+    ['.items-end', 'img', 'svg', 'button', ':empty'].forEach(selector => {
+      template.content.querySelectorAll(selector).forEach(node => {
+        node.remove();
+      });
     });
-  });
-  a.href = URL.createObjectURL(new Blob([`<!DOCTYPE html>
+    a.href = URL.createObjectURL(new Blob([`<!DOCTYPE html>
 <html>
 <head>
   <title>Chat GPT: ${title}</title>
@@ -45,11 +46,14 @@ body > .w-full:nth-child(2n+2) {
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.7.0/build/styles/default.min.css"/>
 </head>
 <body>${template.innerHTML}</body></html>`], {type: 'text/html'}));
-  const reg1 = /[^\p{L}\p{N}]+/gu;
-  const reg2 = /(^-)|(-$)/g;
-  a.download = title.toLowerCase().replace(reg1, "-").replace(reg2, '') + '.html';
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-  URL.revokeObjectURL(a.href);
+    const reg1 = /[^\p{L}\p{N}]+/gu;
+    const reg2 = /(^-)|(-$)/g;
+    a.download = title.toLowerCase().replace(reg1, "-").replace(reg2, '') + '.html';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(a.href);
+  } catch(e) {
+    alert(e.message);
+  }
 })()
